e15bc3485892ce82ff4e337152fe48d228ca3bf6
Yulia <yarantseva89@bk.ru>
2023-06-18 19:05:36 +0600

    Print string massiv

--- Finish_HW/Program.cs
+++ Finish_HW/Program.cs
@@ -9,23 +9,32 @@ void Main()
     Console.WriteLine("Введите количество элементов в массиве:");
     int s;
     if (int.TryParse(Console.ReadLine(), out s) && s > 0)
-        {
+    {
         int num = s;
-        }
+    }
     else Console.WriteLine("This is not a number! Try again!");
     string [] str = new string[s];
     FillString(str);
+    PrintString(str);
 }
 
 void FillString(string [] str)
 {
-  for (int i = 0; i < str.Length; i++)
-  {
-    Console.Write($"Введите {i + 1} элемент массива: ");
-    str [i] = Console.ReadLine();
-    if(str[i] == "")
+    for (int i = 0; i < str.Length; i++)
     {
-      i--;
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        str [i] = Console.ReadLine();
+        if(str[i] == "")
+            {
+                i--;
+            }
     }
-  }
+}
+
+void PrintString(string [] str)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        Console.Write("{0} ", str[i]);
+    }
 }
