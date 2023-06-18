26c5aa31f7c94e393e85df9da62df587b33f5033
Yulia <yarantseva89@bk.ru>
2023-06-18 18:57:18 +0600

    Input size massiv

---
+++ Finish_HW/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам.
+
+
+Main();
+
+void Main()
+{
+  Console.WriteLine("Введите количество элементов в массиве:");
+  int s;
+  if (int.TryParse(Console.ReadLine(), out s) && s > 0)
+  {
+     int num = s;
+  }
+  else Console.WriteLine("This is not a number! Try again!");
+  string [] str = new string[s];
+  
+}
